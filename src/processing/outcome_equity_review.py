4cf48ca3047cd4ab9a0a915b65d27609bcfacd40
Julia McCauley <juliamccauley@julias-mbp-50.mynetworksettings.com>
2023-01-20 21:39:30 -0500

    updated outcome equity

--- src/processing/outcome_equity_review.py
+++ src/processing/outcome_equity_review.py
@@ -7,6 +7,7 @@ TODO:
            these outcomes and see if there are relationships between certain characteristics of Applicants and their outcomes. It will use its
            findings to ensure equitable outcomes as defined below:
            
+           TODO: this can be changed later of course, just an example based on Prof. Netter's comment
            In it's mission statement, HireSense defines equity as "equality of opportunity" meaning all Applicants should have an equal opportunity
            to find a job, apply to a job, advance in the interview process, and get the job.
 
@@ -14,5 +15,15 @@ TODO:
 
     Students will need to choose how to process and represent data that is ordinarily collected and used to assist in hiring to then 
     analyze and attempt to better hiring practices. They will need to decide what kind of characteristics should they include to try to 
-    provide equal opportunity across, how to report this to a Company in a way that will be useful, 
+    provide equal opportunity across, how to report this to a Company in a way that will be work toward the goal of equitable outcomes, and
+    how much detail they should give a Company about potentially sensitive Applicant information.
+
+    In some ways, this is a much more simple data processing task, the complexity is in the choices about what characteristics to use to 
+    measure equitable outcomes and how much information to give a Company to ensure the information actually betters hiring practices while
+    not handing over identifying or sensitive user data.
+
+    TODO:
+    This could incorporate user feedback about the hiring process, if we wanted to do this we'd like to have that as an option for students
+    we could provide a class they could fill in OR provide the class for them to cutdown on the ways to go off the deep end (like having way
+    too much info asked for in the form)
 '''
