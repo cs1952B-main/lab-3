f7fe149b8f85e177dcc96a18a84d53c5a5d200ce
Frank Carber <116412103+frankcarber@users.noreply.github.com>
2024-01-23 15:19:17 -0500

    Update simple_search.py
    Updating variable names from employer to position throughout

--- src/processing/simple_search.py
+++ src/processing/simple_search.py
@@ -2,20 +2,20 @@ from data.helper_classes import *
 '''
     FUNCTIONALITY REQUIREMENTS:
 
-    REQ-A: Applicants should be able to search for jobs. Since each Employer may only post 1 job, Applicants are essentially searching in the 
-        subspace of Employers. Regardless of how this is implemented, it should be a useful tool for helping Applicants to sift through all 
-        of the Employers who have posted jobs on the platform and try to find jobs that match their preferences and profile.
+    REQ-A: Applicants should be able to search for jobs. Since each Position represents a single job, Applicants are essentially searching in the 
+        subspace of Positions. Regardless of how this is implemented, it should be a useful tool for helping Applicants to sift through all 
+        of the Positions on the platform and try to find jobs that match their preferences and profile.
 
-    REQ-B: Employers should be able to search for Applicants. This would allow Employers to reach out to these Applicants to encourage them to 
-        apply for their job posting. Regardless of how this is implemented, it should be a useful tool for helping Employers to search for and
-        find Applicants that would be a good fit for their job posting based on the job's requirements and description.
+    REQ-B: Positions should be able to search for Applicants. This would allow the hiring manager for these Positions to reach out to these Applicants to 
+    encourage them to apply for their job posting. Regardless of how this is implemented, it should be a useful tool for helping Positions to search for and
+    find Applicants that would be a good fit for their job posting based on the job's requirements and description.
 
     STEP 4-A:
-        For this task you will need to choose how to implement simple search for applicants and employers. In addition to the data classes
+        For this task you will need to choose how to implement simple search for applicants and positions. In addition to the data classes
         you've already looked at, we've provided most of the implementation for simple search below. Your task is to choose between two methods
         of implementation: keyword search and full-text search. Be sure to think about --why-- you're making the choice you are and consider
         what it will mean for different types of users and the power dynamics present on a hiring/jobs platform. You will need to select a 
-        method for both the applicant search and the employer search, they do --not-- need to be the same.
+        method for both the applicant search and the position search, they do --not-- need to be the same.
 
         KEYWORD SEARCH: There is map (or dictionary) of different categories of keyword to filter the search results by and pre-selected values
                         for those categories that users can click on to filter their search results. To search, users pick value(s) for whichever
@@ -24,11 +24,11 @@ from data.helper_classes import *
                         Challenges/Considerations: 
                             - Could we accidentally filter out jobs by having limited values for the keyword categories?
                             - Is the list of keywords fixed or should we intelligently generate them?
-                            - Do we create keywords using data from the existing applicants/employers on the platform?
+                            - Do we create keywords using data from the existing applicants/positions on the platform?
                             - Who supplies the keywords?
 
         FULL-TEXT SEARCH: Users simply enter a query into a search bar, stop words are filtered out, and then the search is matched against the
-                          text of data for Applicants or Employees for similarity. Most relevant results as determined by term frequency/relevancy
+                          text of data for Applicants or Positions for similarity. Most relevant results as determined by term frequency/relevancy
                           to the prompt as returned.
 
                           Challenges/Considerations
@@ -49,7 +49,7 @@ class SimpleSearch:
         "degree_level" : ["below highschool", "highschool diploma", "associates degree", "bachelors degree", "masters", "phd and above"]
     }
 
-    EMPLOYER_SEARCH_KEYWORD_DICT = {
+    POSITION_SEARCH_KEYWORD_DICT = {
         "field" : ["Medicine", "Tech", "Business", "Finance", "Law", "Administration", "Education", "Other"],
         "years_of_experience" : ["0-1", "2-5", "5-9", "10+"],
         "degree_level" : ["below highschool", "highschool diploma", "associates degree", "bachelors degree", "masters", "phd and above"],
@@ -71,10 +71,10 @@ class SimpleSearch:
         results = ...
         return results
 
-    #TODO: Choose either keyword or full text for the search feature applicants will use to search for employers/jobs to satisfy REQ-A
-    def applicant_search_for_employer(self, applicant : Applicant, all_employers : list[Employer]):
+    #TODO: Choose either keyword or full text for the search feature applicants will use to search for positions/jobs to satisfy REQ-A
+    def applicant_search_for_position(self, applicant : Applicant, all_positions : list[Position]):
         pass
 
-    #TODO: Choose either keyword or full text for the search feature employers will use to search for applicants to satisfy REQ-B
-    def employer_search_for_applicants(self, employer : Employer, all_potential_applicants : list[Applicant]):
+    #TODO: Choose either keyword or full text for the search feature hiring managers for positions will use to search for applicants to satisfy REQ-B
+    def position_search_for_applicants(self, position : Position, all_potential_applicants : list[Applicant]):
         pass
